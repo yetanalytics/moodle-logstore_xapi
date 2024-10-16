ffcc73027ea4181abec96a3a25d9563b248ea09e
Milton Reder <milt@yetanalytics.com>
2024-10-16 09:07:42 -0400

    remove mod_feedback custom cmv

--- src/transformer/get_event_function_map.php
+++ src/transformer/get_event_function_map.php
@@ -79,7 +79,7 @@ function get_event_function_map() {
         '\mod_facetoface\event\course_module_viewed' => 'all\course_module_viewed',
         '\mod_facetoface\event\signup_success' => 'mod_facetoface\signup_success',
         '\mod_facetoface\event\take_attendance' => 'mod_facetoface\take_attendance',
-        '\mod_feedback\event\course_module_viewed' => 'mod_feedback\course_module_viewed',
+        '\mod_feedback\event\course_module_viewed' => 'all\course_module_viewed',
         '\mod_feedback\event\response_submitted' => 'mod_feedback\response_submitted\handler',
         '\mod_folder\event\course_module_viewed' => 'all\course_module_viewed',
         '\mod_forum\event\course_module_viewed' => 'mod_forum\course_module_viewed',
